1fab32fc07ca6bc9d6c33e2cce9a033deb72f542
Nikhil Kumar Gupta <118952235+nkgup2349@users.noreply.github.com>
2024-03-30 00:47:58 +0530

    added 2 file

---
+++ a.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+cout<<"hello be"<<endl
+// int n; 
+// cin>>n ; 
+// cout<<"Your name is :falana dhemkana:"<<endl
+
+// cout<<"Your age is :"<<n<<endl;
+return 0; 
+}
