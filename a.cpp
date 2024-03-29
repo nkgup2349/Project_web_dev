e0d40461a75d74978ed605539794303ff9aa9a02
Nikhil Kumar Gupta <118952235+nkgup2349@users.noreply.github.com>
2024-03-30 00:49:40 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -3,11 +3,11 @@ using namespace std;
 
 int main(){
 
-cout<<"hello be"<<endl
+cout<<"hello be"<<endl;
 // int n; 
 // cin>>n ; 
-// cout<<"Your name is :falana dhemkana:"<<endl
-
+// cout<<"Your name is :falana dhemkana:"<<endl;
+cout<<"your name is hero chai wala "<<endl;
 // cout<<"Your age is :"<<n<<endl;
 return 0; 
 }
