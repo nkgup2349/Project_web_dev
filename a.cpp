d068cb880f337426d94ed9563a69fe4fdca3d46e
Nikhil Kumar Gupta <118952235+nkgup2349@users.noreply.github.com>
2024-03-30 01:37:47 +0530

    Update a.cpp

--- a.cpp
+++ a.cpp
@@ -1,13 +1,23 @@
-#include<iostream>
-using namespace std;
+// #include<iostream>
+// using namespace std;
 
-int main(){
+// int main(){
 
-cout<<"hello be"<<endl;
-// int n; 
-// cin>>n ; 
-// cout<<"Your name is :falana dhemkana:"<<endl;
-cout<<"your name is hero chai wala "<<endl;
-// cout<<"Your age is :"<<n<<endl;
-return 0; 
+// cout<<"hello be"<<endl;
+// // int n; 
+// // cin>>n ; 
+// // cout<<"Your name is :falana dhemkana:"<<endl;
+// cout<<"your name is hero chai wala "<<endl;
+// // cout<<"Your age is :"<<n<<endl;
+// return 0; 
+// }
+
+
+// Your First C++ Program
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!";
+    return 0;
 }
