fc5033c7b35530f82211440f9244879e8dca51eb
Nikhil Kumar Gupta <118952235+nkgup2349@users.noreply.github.com>
2024-03-30 22:17:40 +0530

    .

--- a.cpp
+++ a.cpp
@@ -9,6 +9,7 @@ cin>>n ;
 cout<<"Your name is :falana dhemkana:"<<endl;
 cout<<"your name is hero chai wala "<<endl;
 cout<<"Your age is :"<<n<<endl;
+cout<<"teri poni ki jai shree ram:"<<endl;
 return 0; 
 }
 
