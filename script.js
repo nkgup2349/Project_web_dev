953a0d33878bc98bca6a6aa66377e4f846161ed9
Nikhil Kumar Gupta <118952235+nkgup2349@users.noreply.github.com>
2024-03-31 21:51:40 +0530

    .

--- script.js
+++ script.js
@@ -1,18 +1,13 @@
-// alert("helllo be ");
+let res = document.getElementById("result");
 
-// let aa=prompt("You want to which type of theme : d for dark and l for light")
-// if(aa=="ww"){
-    // document.body.style.backgroundColor="white";
-    // document.getElementById("AC").backgroundColor="white";
-    // document.getElementById("button_back").style.backgroundColor="white";
-    // document.getElementById("main_outer").style.border="3px solid black"
- 
-// }
+function myvalue(v){
+    res.value+= v;
+} 
+function result(){
+    res.value=eval(res.value);   
+}
 
-// let  a = prompt("enter your age ");
-if(a>=18){
-    alert("You are adult : you can enter this website:");
+function cls(){
+    res.value = "";
 }
-else{
-    alert("You are minor ans you are not authorized to enter in this webside");
-}
+
