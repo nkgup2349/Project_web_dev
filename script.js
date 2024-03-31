126ca92d3efdfbc3894b395069ded87b9c961db8
Nikhil Kumar Gupta <118952235+nkgup2349@users.noreply.github.com>
2024-03-31 12:41:04 +0530

    new file

---
+++ script.js
@@ -0,0 +1,15 @@
+// alert("helllo be ");
+
+let aa=prompt("You want to which type of theme : d for dark and l for light")
+if(aa=="l"){
+    document.body.style.backgroundColor="red";
+    document.body.style.backgroundColor = "red";
+}
+
+let  a = prompt("enter your age ");
+if(a>=18){
+    alert("You are adult : you can enter this website:");
+}
+else{
+    alert("You are minor ans you are not authorized to enter in this webside");
+}
